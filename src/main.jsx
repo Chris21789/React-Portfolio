324b6fe23ffb806643fc89e526b938ed36dada68
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-25 08:26:43 -0400

    installed the dependencies for axios, bootstrap and router. changed the title of the project. created the base page files for the app. set up the router paths in the main file. created the outlet in the app file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { createBrowerRoute, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import Error from './pages/Error'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Work from './pages/Work'
+
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowerRoute([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Work',
+        element: <Work />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
