a57a53e688a95799ca4aefd77be18d3cfce22cab
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-25 12:57:14 -0400

    testing github logo.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer() {
   return (
     <nav id='footer'>
-        <a href='https://github.com/Chris21789'>Github</a>
+        <a href='https://github.com/Chris21789'><img src='../assets/github-mark.svg' /></a>
         <a href='https://www.linkedin.com/in/christopher-colon-7b5942253/'>LinkedIn</a>
     </nav>
   );
